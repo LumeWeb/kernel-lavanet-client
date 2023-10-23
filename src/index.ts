b369ef0be0080a11ac9efc0af3a1aa7e92fccbd7
Derrick Hammer <derrick@derrickhammer.com>
2023-10-23 13:21:44 -0400

    fix: update return type

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Client, factory } from "@lumeweb/libkernel/module";
 export const MODULE = "zrjTW4BiiJV7U8toTwqEMyzRbBy5G1nkdVRc34nW8LTmkEf";
 
 export class LavaNetClient extends Client {
-  public async query(chain: string, query: any): Promise<number> {
+  public async query(chain: string, query: any): Promise<any> {
     return this.callModuleReturn("query", { chain, query });
   }
 }
